6dd63e8623cbb131936f7db6c818d32760af37aa
Yurochka <yurii.yurochka@gmail.com>
2024-02-21 20:37:07 +0200

    feat: set up MongoDB, settings, env vars, MongoDBClient, lifespan

--- backend/src/main.py
+++ backend/src/main.py
@@ -1,6 +1,26 @@
+from contextlib import asynccontextmanager
+from typing import AsyncGenerator
+
 from fastapi import FastAPI
 
-app = FastAPI()
+from .api.views import router as api_router
+from .db.utils import get_mongodb_client
+from .pet_project.settings import settings
+
+
+@asynccontextmanager
+async def lifespan(app: FastAPI) -> AsyncGenerator[None, None]:
+    # set up MongoDB
+    client = get_mongodb_client()
+    db = client.get_database(settings.MONGO_DB_DB)
+    app.mongodb = db  # type: ignore[attr-defined]
+    yield
+    # close MongoDB connection
+    client.close()
+
+
+app = FastAPI(lifespan=lifespan)
+app.include_router(api_router)
 
 
 @app.get("/")
